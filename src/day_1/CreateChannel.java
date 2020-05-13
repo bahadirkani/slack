8063ed54e9ae3531dfa3772f1f2ff0a11b4ad206
bahadirkani <bahadirbektas.gmail.com>
2020-05-13 16:26:36 +0100

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,9 +4,9 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         System.out.println("User is able to create channels");
-
+        int y=5;
+        if (y==5){
+            System.out.println("equal");
+        }
     }
-
-
-
 }
